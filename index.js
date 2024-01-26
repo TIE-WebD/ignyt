abc6db3d143c3752db943c75c0e975b14474d32f
Deepika <nagshree127@gmail.com>
2024-01-26 21:31:38 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "./index.html");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "./index.html");
+});
 
-// app.post("/submit", (req, res) => {
-//   console.log(req.body);
-// });
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+});
 
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
-alert ("hi");
+
